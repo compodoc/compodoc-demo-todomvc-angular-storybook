dab0ddf79e0b4df160c00ee81c76e86dbd450a17
Vincent Ogloblinsky <vincent.ogloblinsky@gmail.com>
2022-03-07 14:20:30 +0100

    Initial commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { AboutModule } from './about';
+
+import { APP_ENUMS } from './app-routes.enum';
+import { LOGIN } from './login.routes';
+
+enum APP_ENUM {
+    home = 'home',
+}
+
+export const APP_ROUTES: Routes = [
+    ...LOGIN,
+    {
+        path: 'about',
+        loadChildren: (): Promise<AboutModule> => import('./about/about.module').then((m) => m.AboutModule),
+    },
+    //{ path: 'about', loadChildren: './about/about.module#AboutModule' },
+    { path: '', redirectTo: APP_ENUMS.home, pathMatch: 'full' },
+    { path: '**', redirectTo: APP_ENUM.home, pathMatch: 'full' },
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(APP_ROUTES)],
+    exports: [RouterModule],
+})
+export class AppRoutingModule {}
